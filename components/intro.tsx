4298499a2353bb512b661c36fed0c1ea811f30fd
Schlumen <lumen.haendler@web.de>
2024-01-25 10:40:05 +0100

    Finish intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -52,6 +56,41 @@ export default function Intro() {
         enjoy building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React, Next.js and Node.js</span>
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}>
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full font-medium outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+          Contact me here
+          <BsArrowRight className="opacity-80 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10"
+          href="/Lumen_CV.pdf"
+          download>
+          Download CV{" "}
+          <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/l-haendler/"
+          target="_blank">
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/Schlumen"
+          target="_blank">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
