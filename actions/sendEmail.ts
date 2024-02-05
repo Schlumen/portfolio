d296f55f01444eeb294c36e994b97c8a95544bd2
Schlumen <lumen.haendler@web.de>
2024-02-05 10:41:29 +0100

    Complete email functionality

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.MAILGUN_API_KEY);
 
@@ -22,17 +24,25 @@ export const sendEmail = async (formData: FormData) => {
     };
   }
 
+  let data;
   try {
-    await resend.emails.send({
+    data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "lumen@haendler.dev",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
     };
   }
+
+  return {
+    data,
+  };
 };
